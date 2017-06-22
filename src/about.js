ae99aff351c04bad7b622a8fdfb6cc0b13c94ec1
ecoulson <ivansonofcoul@gmail.com>
2017-06-22 03:37:30 -0700

    finished about page on desktop need mobile responsive design for sponsor

--- src/about.js
+++ src/about.js
@@ -2,6 +2,9 @@ import './navbar';
 import './css/bootstrap/bootstrap.min.css';
 import './css/about.scss';
 import $ from 'jquery';
+import axios from 'axios';
+
+const youtubeURL = 'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCvou9yaekveOZsyzGQljrNA&maxResults=8&key=AIzaSyB3Z7lmRzaqRwPzoVtzks2ZaSQKABmMGQM';
 
 import {
 	handleSiteTitleAnimation,
@@ -26,10 +29,15 @@ $(document).ready(() => {
 	const $employeeContainer = $('.our-people');
 	const $sponsorContainer = $('.sponsors-container');
 	const $sponsors = $('.sponsor');
+	const $videoContainer = $('.video-container');
+	const $livestreamContainer = $('.livestream-container');
+	const $flexButtons = $('.flex-btns');
 	sizeSponsorImages($sponsors);
 
 	handleHeaderAnimations($siteTitle, $siteDesc, $navbar, $backgroundImage);
 	addSponsorListeners($sponsors);
+	displayYoutubeVideos($videoContainer);
+	handleVideoView($flexButtons, $videoContainer, $livestreamContainer);
 
 	$(window).scroll(() => {
 		let scrollBottom = $(window).scrollTop() + window.innerHeight;
@@ -52,58 +60,93 @@ $(document).ready(() => {
 	});
 });
 
+function handleVideoView(flexButtons, videoContainer, livestreamContainer) {
+	const [ btn1, btn2 ] = Array.from(flexButtons.children());
+	$(btn1).click(() => {
+		$(btn2).removeClass('active');
+		$(btn1).addClass('active');
+		livestreamContainer.slideUp(500);
+		setTimeout(function () {
+			videoContainer.slideDown(500);
+		}, 500);
+	});
+	$(btn2).click(() => {
+		$(btn1).removeClass('active');
+		$(btn2).addClass('active');
+		videoContainer.slideUp(500);
+		setTimeout(function () {
+			livestreamContainer.slideDown(500);
+		}, 500);
+	});
+}
+
 function addSponsorListeners(sponsors) {
 	if (window.innerWidth > 960) {
 		addDesktopSponsorListeners(sponsors);
 	} else {
-		addDesktopSponsorListeners(sponsors);
-		// addMobileSponsorListeners(sponsors);
+		addMobileSponsorListeners(sponsors);
 	}
 }
 
-// function addMobileSponsorListeners(sponsors) {
-// 	sponsors.click((e) => {
-// 		const $overlays = $('.overlay');
-// 		animateOpacity($overlays, 0, 0.5);
-// 		animate($overlays, 'margin-top', 0, 0.5);
-//
-// 		let $overlay = $(e.target).find('.overlay');
-// 		animateOpacity($overlay, 1, 0.5);
-// 		animate($overlay, 'margin-top', -($overlay.height()), 0.5);
-// 	});
-// }
-
-function addDesktopSponsorListeners(sponsors) {
-	sponsors.mouseenter((e) => {
+function addMobileSponsorListeners(sponsors) {
+	let secondClick = false;
+	sponsors.click((e) => {
 		let $overlay = $(e.target).find('.overlay');
 		if ($overlay.is('p')) {
 			$overlay = $overlay.parent();
 		}
-		$overlay.show();
-		animateOpacity($overlay, 1, '0.5s');
-		animate($overlay, 'margin-top', -($overlay.height()), '0.5s');
-	});
-	sponsors.mouseleave((e) => {
-		let $overlay = $(e.target);
-		let cls = $overlay.prop('class');
-		if (cls == 'sponsor') {
-			$overlay = $(e.target).find('.overlay');
-		} else if (cls != 'overlay') {
-			$overlay = $(e.target).parent();
+		if (!secondClick) {
+			showSponsor(e);
+		} else {
+			let sponsors = Array.from($('.sponsor'));
+			sponsors.forEach((sponsor) => {
+				hideSponsor({
+					target: sponsor,
+				});
+			});
+			gotoSponsor(e);
 		}
-		$overlay.hide();
-		animateOpacity($overlay, 0, '0.5s');
-		animate($overlay, 'margin-top', '0px', '0.5s');
 	});
+}
 
-	sponsors.click((e) => {
-		let elem = $(e.target);
-		while(elem.prop('class') != 'sponsor') {
-			elem = elem.parent();
-		}
-		let url = elem.data('link');
-		window.location.href = url;
-	});
+function addDesktopSponsorListeners(sponsors) {
+	sponsors.mouseenter(showSponsor);
+	sponsors.mouseleave(hideSponsor);
+	sponsors.click(gotoSponsor);
+}
+
+function gotoSponsor(e) {
+	console.log(e);
+	let elem = $(e.target);
+	while(elem.prop('class') != 'sponsor') {
+		elem = elem.parent();
+	}
+	let url = elem.data('link');
+	window.location.href = url;
+}
+
+function hideSponsor(e) {
+	let $overlay = $(e.target);
+	let cls = $overlay.prop('class');
+	if (cls == 'sponsor') {
+		$overlay = $(e.target).find('.overlay');
+	} else if (cls != 'overlay') {
+		$overlay = $(e.target).parent();
+	}
+	$overlay.hide();
+	animateOpacity($overlay, 0, '0.5s');
+	animate($overlay, 'margin-top', '0px', '0.5s');
+	return $overlay;
+}
+
+function showSponsor(e) {
+	let $overlay = $(e.target).find('.overlay');
+	if ($overlay.is('p')) {
+		$overlay = $overlay.parent();
+	}
+	$overlay.show();
+	animateOpacity($overlay, 1, '0.5s');
+	animate($overlay, 'margin-top', -($overlay.height()), '0.5s');
 }
 
 function animateContainer(container, scrollBottom, selector, animations) {
@@ -144,6 +187,55 @@ function sizeSponsorImages($sponsors) {
 			$(sponsor).find('.overlay').css({
 				'height': height / width * $(sponsor).width(),
 			});
+		});
+	});
+}
+
+function displayYoutubeVideos($videoContainer) {
+	axios.get(youtubeURL)
+		.then((res) => {
+			return res.data;
+		})
+		.then((data) => {
+			return data.items.reduce((x, res) => {
+				x.push({
+					id: res.id.videoId,
+					title: res.snippet.title,
+					thumbnail: res.snippet.thumbnails.high.url,
+				});
+				return x;
+			}, []);
+		})
+		.then((videos) => {
+			let str = videos.reduce((x, video) => {
+				return x + `<div data-id="${video.id}" class="video"><h6 class="title">${video.title}</h6><img src="${video.thumbnail}" class="thumbnail"/><img src="../public/imgs/youtube.svg" alt="play icon" class="icon"/></div>`;
+			}, '');
+			$videoContainer.append(str);
+			let timer = setInterval(function () {
+				if (Array.from($videoContainer.children()).length > 0) {
+					clearInterval(timer);
+					addLoadHandlers($videoContainer);
+				}
+			}, 10);
+		});
+}
+
+function addLoadHandlers(container) {
+	let videos = Array.from(container.children());
+	videos.forEach((video) => {
+		$(video).click((e) => {
+			let $target = $(e.target);
+			while ($target.prop('class') != 'video') {
+				$target = $target.parent();
+			}
+			let id = $target.data('id');
+			let $thumbnail = $target.find('.thumbnail');
+			let height = $thumbnail.height();
+			let width = $thumbnail.width();
+			let html = `<iframe style="background-color: black" height="${height}" width="${width}" src="https://www.youtube.com/embed/${id}?autoplay=1" frameborder="0" allowfullscreen></iframe>`;
+			$thumbnail.remove();
+			$target.find('.icon').remove();
+			$target.append(html);
 		});
 	});
 }
